8557f91d6f7f3db81924f3b1c6237deb8f343c23
semrubo <semrubo2gmail.com>
2015-09-10 14:12:53 +0400

    Added LinphoneVideoPolicy wrapper

---
+++ VATRP.LinphoneWrapper/Structs/LinphoneVideoPolicy.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace VATRP.LinphoneWrapper.Structs
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct LinphoneVideoPolicy
+    {
+        public bool automatically_initiate; /**<Whether video shall be automatically proposed for outgoing calls.*/
+        public bool automatically_accept; /**<Whether video shall be automatically accepted for incoming calls*/
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)] public bool[] unused;
+    }
+}
