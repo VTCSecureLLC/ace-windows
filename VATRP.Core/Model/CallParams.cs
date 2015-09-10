5cd3e5c841117f834d82bfa6656492835c848cb0
semrubo <semrubo2gmail.com>
2015-09-10 13:30:39 +0400

    Added CallParams structure for calls

---
+++ VATRP.Core/Model/CallParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VATRP.Core.Model
+{
+    public class CallParams
+    {
+        public string DisplayName;
+        public string Username;
+        public string HostAddress;
+        public int HostPort;
+        
+    }
+}
