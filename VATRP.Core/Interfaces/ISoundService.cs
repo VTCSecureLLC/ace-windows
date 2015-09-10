20b88d90632780c8957d0fa09986d31351c46338
semrubo <semrubo2gmail.com>
2015-09-10 13:33:38 +0400

    Added interface definitions - PlayRingTone - StopRingTone - PlayRingBackTone - StopRingBackTone - PlayNewEvent - StopNewEvent - PlayConnectionChanged - StopConnectionChanged

--- VATRP.Core/Interfaces/ISoundService.cs
+++ VATRP.Core/Interfaces/ISoundService.cs
@@ -3,6 +3,17 @@ namespace VATRP.Core.Interfaces
 {
     public interface ISoundService : IVATRPservice
     {
+        void PlayRingTone();
+        void StopRingTone();
 
+        void PlayRingBackTone();
+
+        void StopRingBackTone();
+        void PlayNewEvent();
+
+        void StopNewEvent();
+
+        void PlayConnectionChanged(bool connected);
+        void StopConnectionChanged(bool connected);
     }
 }
