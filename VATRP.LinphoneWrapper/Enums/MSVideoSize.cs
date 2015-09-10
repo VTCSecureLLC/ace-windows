6fe7a099f590b32c14069a0992333291031ccd6b
semrubo <semrubo2gmail.com>
2015-09-10 13:36:32 +0400

    Added more video size definitions

--- VATRP.LinphoneWrapper/Enums/MSVideoSize.cs
+++ VATRP.LinphoneWrapper/Enums/MSVideoSize.cs
@@ -2,6 +2,106 @@
 {
     public enum MSVideoSize
     {
-        MSVideoSizeSmall
+        MS_VIDEO_SIZE_UNKNOWN_W = 0,
+        MS_VIDEO_SIZE_UNKNOWN_H = 0,
+
+        MS_VIDEO_SIZE_SQCIF_W = 128,
+        MS_VIDEO_SIZE_SQCIF_H = 96,
+
+        MS_VIDEO_SIZE_WQCIF_W = 256,
+        MS_VIDEO_SIZE_WQCIF_H = 144,
+
+        MS_VIDEO_SIZE_QCIF_W = 176,
+        MS_VIDEO_SIZE_QCIF_H = 144,
+
+        MS_VIDEO_SIZE_CIF_W = 352,
+        MS_VIDEO_SIZE_CIF_H = 288,
+
+        MS_VIDEO_SIZE_CVD_W = 352,
+        MS_VIDEO_SIZE_CVD_H = 480,
+
+        MS_VIDEO_SIZE_ICIF_W = 352,
+        MS_VIDEO_SIZE_ICIF_H = 576,
+
+        MS_VIDEO_SIZE_4CIF_W = 704,
+        MS_VIDEO_SIZE_4CIF_H = 576,
+
+        MS_VIDEO_SIZE_W4CIF_W = 1024,
+        MS_VIDEO_SIZE_W4CIF_H = 576,
+
+        MS_VIDEO_SIZE_QQVGA_W = 160,
+        MS_VIDEO_SIZE_QQVGA_H = 120,
+
+        MS_VIDEO_SIZE_HQVGA_W = 160,
+        MS_VIDEO_SIZE_HQVGA_H = 240,
+
+        MS_VIDEO_SIZE_QVGA_W = 320,
+        MS_VIDEO_SIZE_QVGA_H = 240,
+
+        MS_VIDEO_SIZE_HVGA_W = 320,
+        MS_VIDEO_SIZE_HVGA_H = 480,
+
+        MS_VIDEO_SIZE_VGA_W = 640,
+        MS_VIDEO_SIZE_VGA_H = 480,
+
+        MS_VIDEO_SIZE_SVGA_W = 800,
+        MS_VIDEO_SIZE_SVGA_H = 600,
+
+        MS_VIDEO_SIZE_NS1_W = 324,
+        MS_VIDEO_SIZE_NS1_H = 248,
+
+        MS_VIDEO_SIZE_QSIF_W = 176,
+        MS_VIDEO_SIZE_QSIF_H = 120,
+
+        MS_VIDEO_SIZE_SIF_W = 352,
+        MS_VIDEO_SIZE_SIF_H = 240,
+
+        MS_VIDEO_SIZE_IOS_MEDIUM_W = 480,
+        MS_VIDEO_SIZE_IOS_MEDIUM_H = 360,
+
+        MS_VIDEO_SIZE_ISIF_W = 352,
+        MS_VIDEO_SIZE_ISIF_H = 480,
+
+        MS_VIDEO_SIZE_4SIF_W = 704,
+        MS_VIDEO_SIZE_4SIF_H = 480,
+
+        MS_VIDEO_SIZE_288P_W = 512,
+        MS_VIDEO_SIZE_288P_H = 288,
+
+        MS_VIDEO_SIZE_432P_W = 768,
+        MS_VIDEO_SIZE_432P_H = 432,
+
+        MS_VIDEO_SIZE_448P_W = 768,
+        MS_VIDEO_SIZE_448P_H = 448,
+
+        MS_VIDEO_SIZE_480P_W = 848,
+        MS_VIDEO_SIZE_480P_H = 480,
+
+        MS_VIDEO_SIZE_576P_W = 1024,
+        MS_VIDEO_SIZE_576P_H = 576,
+
+        MS_VIDEO_SIZE_720P_W = 1280,
+        MS_VIDEO_SIZE_720P_H = 720,
+
+        MS_VIDEO_SIZE_1080P_W = 1920,
+        MS_VIDEO_SIZE_1080P_H = 1080,
+
+        MS_VIDEO_SIZE_SDTV_W = 768,
+        MS_VIDEO_SIZE_SDTV_H = 576,
+
+        MS_VIDEO_SIZE_HDTVP_W = 1920,
+        MS_VIDEO_SIZE_HDTVP_H = 1200,
+
+        MS_VIDEO_SIZE_XGA_W = 1024,
+        MS_VIDEO_SIZE_XGA_H = 768,
+
+        MS_VIDEO_SIZE_WXGA_W = 1080,
+        MS_VIDEO_SIZE_WXGA_H = 768,
+
+        MS_VIDEO_SIZE_SXGA_MINUS_W = 1280,
+        MS_VIDEO_SIZE_SXGA_MINUS_H = 960,
+
+        MS_VIDEO_SIZE_UXGA_W = 1600,
+        MS_VIDEO_SIZE_UXGA_H = 1200,
     }
 }
