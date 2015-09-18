371f3b34bdc0fcd0cebc20c4c72924858a583de6
semrubo <semrubo2gmail.com>
2015-09-18 23:54:23 +0400

    Added Codecs Settings Control

---
+++ VATRP.App/CustomControls/CodecsSettingsCtrl.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VATRP.App.Interfaces;
+using VATRP.App.Services;
+using VATRP.Core.Model;
+
+namespace VATRP.App.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for CodecsSettingsCtrl.xaml
+    /// </summary>
+    public partial class CodecsSettingsCtrl : ISettings
+    {
+        public CodecsSettingsCtrl()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (App.CurrentAccount == null)
+                return;
+             AudioCodecsListView.Items.Clear();
+            foreach (var item in App.CurrentAccount.AudioCodecsList)
+            {
+                AudioCodecsListView.Items.Add(item);
+            }
+
+            VideoCodecsListView.Items.Clear();
+            foreach (var item in App.CurrentAccount.VideoCodecsList)
+            {
+                VideoCodecsListView.Items.Add(item);
+            }
+        }
+
+        #region ISettings
+
+        public bool IsChanged()
+        {
+            if (App.CurrentAccount == null)
+                return false;
+
+           
+            return true;
+        }
+
+        public bool Save()
+        {
+            if (App.CurrentAccount == null)
+                return false;
+            
+            return true;
+        }
+
+        #endregion
+    }
+}
