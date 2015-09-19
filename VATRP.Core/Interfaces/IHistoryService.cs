345159b0c83fa873d7bbbf6c1d70c1f94acdff66
semrubo <semrubo2gmail.com>
2015-09-19 19:30:51 +0400

    New methods for call history

--- VATRP.Core/Interfaces/IHistoryService.cs
+++ VATRP.Core/Interfaces/IHistoryService.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using VATRP.Core.Events;
+using VATRP.Core.Model;
 
 namespace VATRP.Core.Interfaces
 {
     public interface IHistoryService : IVATRPservice
     {
-       
+        #region Calls
+        bool IsLoadingCalls { get; }
+        
+        int AddCallEvent(VATRPCallEvent callEvent);
+
+        event EventHandler<VATRPCallEventArgs> OnCallHistoryEvent;
+
+        int DeleteCallEvent(string id);
+
+        void ClearCallsItems();
+
+        List<VATRPCallEvent> LoadCallEvents(int limit = -1);
+
+        #endregion
     }
 }
