a38daccb677a7a1db5f8a27286765107682aa51d
semrubo <semrubo2gmail.com>
2015-09-19 19:18:30 +0400

    Moved from MyEventArgs

---
+++ VATRP.Core/Events/VATRPEventArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VATRP.Core.Events
+{
+    public class VATRPEventArgs : EventArgs
+    {
+        private readonly IDictionary<String, Object> extras;
+
+        public VATRPEventArgs()
+            :base()
+        {
+            this.extras = new Dictionary<String, Object>();
+        }
+
+        public Object GetExtra(String key)
+        {
+            if (this.extras.ContainsKey(key))
+            {
+                return this.extras[key];
+            }
+            return null;
+        }
+
+        public VATRPEventArgs AddExtra(String key, Object value)
+        {
+            if (!this.extras.ContainsKey(key))
+            {
+                this.extras.Add(key, value);
+            }
+            return this;
+        }
+    }
+}
