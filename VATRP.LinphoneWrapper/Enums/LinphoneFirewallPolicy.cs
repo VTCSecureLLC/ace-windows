3dcbd42fe4423696dd9b2b03e104ef6bb1b190a2
semrubo <semrubo2gmail.com>
2015-09-19 19:20:57 +0400

    Added LinphoneFirewallPolicy enum

---
+++ VATRP.LinphoneWrapper/Enums/LinphoneFirewallPolicy.cs
@@ -0,0 +1,11 @@
+﻿namespace VATRP.LinphoneWrapper.Enums
+{
+    public enum LinphoneFirewallPolicy
+    {
+        LinphonePolicyNoFirewall, // Do not use any mechanism to pass through firewalls */
+        LinphonePolicyUseNatAddress, // Use the specified public address */
+        LinphonePolicyUseStun, // Use a STUN server to get the public address */
+        LinphonePolicyUseIce, // Use the ICE protocol */
+        LinphonePolicyUseUpnp, // Use the uPnP protocol */
+    }
+}
