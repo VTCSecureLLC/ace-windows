f37681d936ae6fa4f650776cc73eba4383dc8283
semrubo <semrubo2gmail.com>
2015-09-19 19:24:18 +0400

    Future method placeholders for contact service

--- VATRP.Core/Interfaces/IContactService.cs
+++ VATRP.Core/Interfaces/IContactService.cs
@@ -3,5 +3,9 @@
     public interface IContactService : IVATRPservice
     {
 
+        string GetContactDisplayName(Model.VATRPContact contact, string useraname);
+
+        Model.VATRPContact FindContactByUsername(string useraname);
+        Model.VATRPContact FindContactId(string id);
     }
 }
--- VATRP.Core/Services/ContactService.cs
+++ VATRP.Core/Services/ContactService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Xml.Serialization;
-using log4net;
+﻿using log4net;
 using VATRP.Core.Interfaces;
 using VATRP.Core.Model;
 
@@ -13,54 +8,55 @@ namespace VATRP.Core.Services
     {
         private static readonly ILog LOG = LogManager.GetLogger(typeof (ContactService));
 
-        private bool _isStarting = false;
-        private bool _isStarted = false;
-        private bool _isStopping = false;
-        private bool _isStopped = false;
         private readonly ServiceManagerBase manager;
 
         public ContactService(ServiceManagerBase manager)
         {
             this.manager = manager;
-            _isStarting = false;
-            _isStarted = false;
         }
 
-        public bool IsStarting { get { return _isStarting; } }
-
-        public bool IsStarted { get { return _isStarted; } }
-
-        public bool IsStopping
-         {
-            get { return _isStopping; }
-        }
-
-        public bool IsStopped
-        {
-            get { return _isStopped; }
-        }
+        #region IVATRPService
 
         public bool Start()
         {
-            if (IsStarting || IsStarted)
-                return true;
-
-            _isStarting = true;
-            _isStarted = false;
-
             return true;
         }
 
         public bool Stop()
         {
-            if (IsStopping || IsStarting)
-                return false;
-
-            if (IsStopped)
-                return true;
-
-            _isStarted = false;
             return true;
         }
+
+        #endregion
+
+        #region IContactService
+
+        public string GetContactDisplayName(VATRPContact contact, string username)
+        {
+            if (contact != null)
+                return contact.DisplayName;
+
+            string dn = username;
+
+            var c = FindContactByUsername(username);
+            if (c != null)
+            {
+                dn = c.DisplayName ?? c.FullName;
+
+            }
+            return dn;
+        }
+        
+        public VATRPContact FindContactByUsername(string useraname)
+        {
+            return null;
+        }
+
+        public VATRPContact FindContactId(string id)
+        {
+            return null;
+        }
+
+        #endregion
     }
 }
