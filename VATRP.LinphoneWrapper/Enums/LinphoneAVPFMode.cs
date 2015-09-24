e73551ff4bc544ec1b532514c2a017b8126c8ec6
Ruben Semerjyan <semrubo@gmail.com>
2015-09-25 02:07:13 +0400

    Wrapped LinphoneAVPFMode enum

---
+++ VATRP.LinphoneWrapper/Enums/LinphoneAVPFMode.cs
@@ -0,0 +1,9 @@
+﻿namespace VATRP.LinphoneWrapper.Enums
+{
+    public enum LinphoneAVPFMode
+    {
+        LinphoneAVPFDefault = -1, // Use default value defined at upper level
+        LinphoneAVPFDisabled, // AVPF is disabled
+        LinphoneAVPFEnabled // AVPF is enabled 
+    }
+}
