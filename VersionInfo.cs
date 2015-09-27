afbc6c09638d8a1cf34a940c362ac398f58d08aa
zarkara <khdick@gmail.com>
2015-09-27 19:46:30 -0400

    set default version 0.0.0.0

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection; 
 using System.Runtime.CompilerServices; 
 using System.Runtime.InteropServices; 
-[assembly: AssemblyVersion("0.0.28.0")] 
-[assembly: AssemblyFileVersion("0.0.28.0")] 
+[assembly: AssemblyVersion("0.0.0.0")] 
+[assembly: AssemblyFileVersion("0.0.0.0")] 
