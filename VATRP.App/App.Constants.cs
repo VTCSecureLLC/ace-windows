9ddb9a4622708c63edf353c326ebd8c607f7e1b6
zarkara <khdick@gmail.com>
2015-09-27 21:36:48 -0400

    Prepare file for automated replace with hockeyapp key

--- VATRP.App/App.Constants.cs
+++ VATRP.App/App.Constants.cs
@@ -1,13 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace VATRP.App
-{
-    public partial class App
-    {
-        public static string HOCKEYAPP_ID = "73c835b884c82ca5f20548f985486255";
-        public static string HOCKEYAPP_SECRET = "437aeef7a03569ae394b572893472cf3";
-    }
-}
+﻿using System;namespace VATRP.App{ public partial class App {public static string HOCKEYAPP_ID = "00000000000000";}}
