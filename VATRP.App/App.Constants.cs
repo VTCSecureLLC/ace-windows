1493fe9634e4002d52e9fb9ca8ff5b81620a4f32
Ruben Semerjyan <semrubo@gmail.com>
2015-10-06 22:18:36 +0400

    Added hockeyapp ID

--- VATRP.App/App.Constants.cs
+++ VATRP.App/App.Constants.cs
@@ -1,1 +1,2 @@
-﻿using System;namespace VATRP.App{ public partial class App {public static string HOCKEYAPP_ID = "00000000000000";}}
+﻿using System;
+namespace VATRP.App { public partial class App { public static string HOCKEYAPP_ID = "74e8a40751df963f6c8da758b41e4f45";} }
