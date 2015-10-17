7c0663e78bc98b4fe7339970233abc0bb8a7dad9
Ruben Semerjyan <semrubo@gmail.com>
2015-10-17 14:22:56 +0400

    Added converter for CallInfoViewModel

---
+++ VATRP.App/Converters/CallQualityConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using VATRP.Core.Model;
+
+namespace VATRP.App.Converters
+{
+    public class CallQualityConverter : IValueConverter
+    {
+        public CallQualityConverter()
+        {
+            
+        }
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is float)
+            {
+                var rating = (float)value;
+                if (rating >= 4.0)
+                    return "Good";
+                if (rating >= 3.0)
+                    return "Average";
+                if (rating >= 2.0)
+                    return "Poor";
+                if (rating >= 1.0)
+                    return "Very poor";
+                if (rating >= 0)
+                    return "Too bad";
+            }
+
+            return "Unavailable";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
