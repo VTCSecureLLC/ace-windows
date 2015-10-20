88e0142f90fcf2a3ddc4721b2ce4c5a7e9005b88
Ruben Semerjyan <semrubo@gmail.com>
2015-10-20 22:19:34 +0400

    Added custom eventargs for messaging service

---
+++ VATRP.Core/Events/TypingInChatEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using VATRP.Core.Enums;
+using VATRP.Core.Model;
+
+namespace VATRP.Core.Events
+{
+    public class TypingInChatEventArgs : EventArgs
+    {
+        public TypingInChatEventArgs(VATRPChat chat, TypingAction action)
+        {
+            this.Chat = chat;
+            this.Action = action;
+        }
+
+        public TypingAction Action { get; private set; }
+
+        public VATRPChat Chat { get; private set; }
+    }
+}
+
