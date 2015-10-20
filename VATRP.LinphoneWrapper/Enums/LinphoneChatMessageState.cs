146e6f2854174f57db15670c94d7081ca255fcf8
Ruben Semerjyan <semrubo@gmail.com>
2015-10-20 22:32:45 +0400

    Added LinphoneChatMessageState enum

---
+++ VATRP.LinphoneWrapper/Enums/LinphoneChatMessageState.cs
@@ -0,0 +1,12 @@
+﻿namespace VATRP.LinphoneWrapper.Enums
+{
+    public enum LinphoneChatMessageState
+    {
+        LinphoneChatMessageStateIdle, /**< Initial state */
+        LinphoneChatMessageStateInProgress, /**< Delivery in progress */
+        LinphoneChatMessageStateDelivered, /**< Message successfully delivered and acknowledged by remote end point */
+        LinphoneChatMessageStateNotDelivered, /**< Message was not delivered */
+        LinphoneChatMessageStateFileTransferError, /**< Message was received(and acknowledged) but cannot get file from server */
+        LinphoneChatMessageStateFileTransferDone /**< File transfer has been completed successfully. */
+    }
+}
