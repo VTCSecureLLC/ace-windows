8a803b66b7e861859fa6b2df0f035113e0368b70
Ruben Semerjyan <semrubo@gmail.com>
2015-10-20 22:32:01 +0400

    Updated ContactService, added more logic for messaging

--- VATRP.Core/Services/ContactService.cs
+++ VATRP.Core/Services/ContactService.cs
@@ -1,24 +1,247 @@
-﻿using log4net;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using VATRP.Core.Enums;
+using VATRP.Core.Events;
 using VATRP.Core.Interfaces;
 using VATRP.Core.Model;
 
 namespace VATRP.Core.Services
 {
-    public partial class ContactService : IContactService
+    public sealed class ContactService : IContactsService
     {
-        private static readonly ILog LOG = LogManager.GetLogger(typeof (ContactService));
-
         private readonly ServiceManagerBase manager;
+        private ObservableCollection<VATRPContact> _contacts;
+        private ObservableCollection<VATRPUserGroup> _groups;
 
+
+        public event EventHandler<ContactEventArgs> ContactAdded;
+        public event EventHandler<ContactRemovedEventArgs> ContactRemoved;
+
+        public event EventHandler<ContactStatusChangedEventArgs> ContactStatusChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event EventHandler<EventArgs> GroupsChanged;
+        public event EventHandler<EventArgs> ContactsChanged;
+
+        public bool IsLoaded { get; private set; }
         public ContactService(ServiceManagerBase manager)
         {
             this.manager = manager;
+            IsLoaded = false;
         }
 
-        #region IVATRPService
+        private void RemoveGroupFromContactList(string _groupName)
+        {
+            lock (Contacts)
+            {
+                foreach (var contact in Contacts)
+                {
+                    if (contact.IsGroupExistInGroupList(_groupName))
+                        contact.RemoveGroupFromGroupList(_groupName);
+                }
+            }
+        }
+
+        private void RemoveGroup(int removed)
+        {
+            lock (this.Groups)
+            {
+                foreach (var gr in Groups)
+                {
+                    if (gr.ID == removed)
+                    {
+                        Groups.Remove(gr);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private VATRPUserGroup FindGroups(int removed)
+        {
+            lock (this.Groups)
+            {
+                foreach (var gr in Groups)
+                {
+                    if (gr.ID == removed)
+                    {
+                        return gr;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void AddContact(VATRPContact contact, string group)
+        {
+            Contacts.Add(contact);
+            if (ContactAdded != null)
+                ContactAdded(this, new ContactEventArgs(new ContactID(contact)));
+        }
+
+        public VATRPContact FindContact(ContactID contactID)
+        {
+            if (contactID == null)
+            {
+                return null;
+            }
+            return this.FindContact(contactID.ID);
+        }
+
+        public VATRPContact FindContact(int contactID)
+        {
+            lock (this.Contacts)
+            {
+                foreach (VATRPContact contact in this.Contacts)
+                {
+                    if ((contact != null) && (contact.ID == contactID.ToString()))
+                    {
+                        return contact;
+                    }
+                }
+            }
+            return null;
+        }
+        
+        public VATRPContact FindLoggedInContact()
+        {
+            lock (this.Contacts)
+            {
+                foreach (VATRPContact contact in this.Contacts)
+                {
+                    if ((contact != null) && contact.IsLoggedIn)
+                    {
+                        return contact;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private VATRPContact FindContact(string id)
+        {
+            lock (this.Contacts)
+            {
+                foreach (var contact in this.Contacts)
+                {
+                    if ((contact != null) && (contact.ID == id))
+                    {
+                        return contact;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void RemoveContact(string id, bool isUserAction)
+        {
+            lock (this.Contacts)
+            {
+                foreach (VATRPContact contact in this.Contacts)
+                {
+                    if ((contact != null) && (contact.ID == id))
+                    {
+                        this.Contacts.Remove(contact);
+                        if (ContactRemoved != null)
+                            ContactRemoved(this, new ContactRemovedEventArgs(new ContactID(contact), isUserAction));
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void RenameContact(ContactID contactID, string nick)
+        {
+
+        }
+
+        private void SaveContactsAsync(ObservableCollection<VATRPContact> observableCollection)
+        {
+            if (this.IsLoaded)
+            {
+                
+            }
+        }
+
+        internal bool SearchContactIfExistOrCreateNew(ContactID contactID, out VATRPContact contact)
+        {
+            bool flag = false;
+            contact = this.FindContact(contactID);
+            if (contact == null)
+            {
+                flag = true;
+                contact = new VATRPContact(contactID);
+            }
+            return flag;
+        }
+
+        internal void SetMail_inViewList(VATRPContact contact, bool mailVisible)
+        {
+            if (contact != null)
+            {
+                VATRPContact contact2 = this.FindContact(contact);
+                if (contact2 != null)
+                {
+                    if (mailVisible)
+                    {
+                        contact2.UnreadMsgCount++;
+                    }
+                    else
+                    {
+                        contact2.UnreadMsgCount = 0;
+                    }
+                }
+            }
+        }
+
+        public ObservableCollection<VATRPContact> Contacts
+        {
+            get
+            {
+                if (this._contacts == null)
+                {
+                    this._contacts = new ObservableCollection<VATRPContact>();
+                }
+                return this._contacts;
+            }
+            private set { this._contacts = value; }
+        }
+
+        public ObservableCollection<VATRPUserGroup> Groups
+        {
+            get
+            {
+                if (this._groups == null)
+                {
+                    this._groups = new ObservableCollection<VATRPUserGroup>();
+                }
+                return this._groups;
+            }
+            private set { this._groups = value; }
+        }
+
+        private string GetGroupName(int groupID)
+        {
+            lock (Groups)
+            {
+                foreach (var group in Groups)
+                {
+                    if (group.ID == groupID)
+                        return group.Name;
+                }
+            }
+
+            return groupID == 0 ? "All" : string.Empty;
+        }
 
         public bool Start()
         {
+            IsLoaded = true;
             return true;
         }
 
@@ -26,37 +249,6 @@ namespace VATRP.Core.Services
         {
             return true;
         }
-
-        #endregion
-
-        #region IContactService
-
-        public string GetContactDisplayName(VATRPContact contact, string username)
-        {
-            if (contact != null)
-                return contact.DisplayName;
-
-            string dn = username;
-
-            var c = FindContactByUsername(username);
-            if (c != null)
-            {
-                dn = c.DisplayName ?? c.FullName;
-
-            }
-            return dn;
-        }
-        
-        public VATRPContact FindContactByUsername(string useraname)
-        {
-            return null;
-        }
-
-        public VATRPContact FindContactId(string id)
-        {
-            return null;
-        }
-
-        #endregion
     }
 }
+
