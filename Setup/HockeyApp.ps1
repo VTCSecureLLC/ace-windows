3d51e1946e9bb56716c830ee32396fd7ac8dd6e1
Ian Blenke <ian@blenke.com>
2015-10-22 23:43:53 -0400

    VATRP-417 - Auto-Create a HockeyApp Version and then Auto-Update after a successful Appveyor build against master branch

--- Setup/HockeyApp.ps1
+++ Setup/HockeyApp.ps1
@@ -1,6 +1,7 @@
 if ($env:APPVEYOR_REPO_BRANCH -ne "master")
 {
  Write-Host 'Skip "HockeyApp" deployment as no branches matched (build branch is "'"$env:APPVEYOR_REPO_BRANCH"'", deploy on branches "master")'
+ exit 0
 }
 
 Get-ChildItem .\Setup\Out\*.msi | ForEach-Object {
@@ -16,8 +17,6 @@ Get-ChildItem .\Setup\Out\*.msi | ForEach-Object {
     -H "X-HockeyAppToken: $env:HOCKEYAPP_TOKEN" `
     https://rink.hockeyapp.net/api/2/apps/$env:HOCKEYAPP_ID/app_versions/new | jq -r .id)
 
-  Write-Host "Created app version $id for $env:APPVEYOR_BUILD_VERSION - $env:APPVEYOR_BUILD_NUMBER"
-
   & C:\ProgramData\Chocolatey\bin\curl -sLk `
     -X PUT `
     -F "ipa=@$msi" `
@@ -28,6 +27,6 @@ Get-ChildItem .\Setup\Out\*.msi | ForEach-Object {
     -F "teams=$env:HOCKEYAPP_TEAM_IDS" `
     -F "mandatory=0" `
     -H "X-HockeyAppToken: $env:HOCKEYAPP_TOKEN" `
-    https://rink.hockeyapp.net/api/2/apps/$env:HOCKEYAPP_ID/app_versions/$id
+    https://rink.hockeyapp.net/api/2/apps/$env:HOCKEYAPP_ID/app_versions/$id | jq .
 }
 
