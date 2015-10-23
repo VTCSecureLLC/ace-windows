b1113ea8d53fe7e052b70898532327e1afc20d9d
Ian Blenke <ian@blenke.com>
2015-10-22 23:22:41 -0400

    VATRP-417 - Create a HockeyApp Version and then Update it

--- Setup/HockeyApp.ps1
+++ Setup/HockeyApp.ps1
@@ -1,25 +1,32 @@
 if ($env:APPVEYOR_REPO_BRANCH -ne "master")
 {
  Write-Host 'Skip "HockeyApp" deployment as no branches matched (build branch is "'"$env:APPVEYOR_REPO_BRANCH"'", deploy on branches "master")'
- exit 0
 }
 
 Get-ChildItem .\Setup\Out\*.msi | ForEach-Object {
   $msi=$_.FullName
 
-  & C:\ProgramData\Chocolatey\bin\curl -sLk `
-    -F "status=2" `
-    -F "notify=1" `
-    -F "commit_sha=$env:APPVEYOR_REPO_COMMIT" `
-    -F "build_server_url=https://ci.appveyor.com/project/$APPVEYOR_ACCOUNT_NAME/$APPVEYOR_PROJECT_NAME/build/$env:APPVEYOR_BUILD_VERSION" `
-    -F "repository_url=http://github.com/$APPVEYOR_REPO_NAME" `
-    -F "release_type=2" `
+  $id=$(& C:\ProgramData\Chocolatey\bin\curl -sLk `
+    -F "bundle_version=$env:APPVEYOR_BUILD_NUMBER"
+    -F "bundle_short_version=$env:APPVEYOR_BUILD_VERSION"
     -F "notes=$APPVEYOR_REPO_COMMIT_MESSAGE" `
     -F "notes_type=1" `
-    -F "mandatory=0" `
+    -F "status=2" `
     -F "teams=$env:HOCKEYAPP_TEAM_IDS" `
-    -F "ipa=@$msi" `
     -H "X-HockeyAppToken: $env:HOCKEYAPP_TOKEN" `
-    https://rink.hockeyapp.net/api/2/apps/$env:HOCKEYAPP_ID/app_versions/upload
+    https://rink.hockeyapp.net/api/2/apps/$env:HOCKEYAPP_ID/app_versions/new | jq -r .id)
+
+  Write-Host "Created app version $id for $APPVEYOR_BUILD_VERSION - $APPVEYOR_BUILD_NUMBER"
+
+  & C:\ProgramData\Chocolatey\bin\curl -sLk `
+    -F "ipa=@$msi" `
+    -F "notes=$APPVEYOR_REPO_COMMIT_MESSAGE" `
+    -F "notes_type=1" `
+    -F "notify=1" `
+    -F "status=2" `
+    -F "teams=$env:HOCKEYAPP_TEAM_IDS" `
+    -F "mandatory=0" `
+    -H "X-HockeyAppToken: $env:HOCKEYAPP_TOKEN" `
+    https://rink.hockeyapp.net/api/2/apps/$env:HOCKEYAPP_ID/app_versions/$id
 }
 
