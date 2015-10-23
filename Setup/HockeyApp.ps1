15849a5270b7c8af17dac1105d621ce4b408c529
Ian Blenke <ian@blenke.com>
2015-10-22 22:57:00 -0400

    VATRP-417 - Auto-upload the MSI to HockeyApp if this is the master branch

--- Setup/HockeyApp.ps1
+++ Setup/HockeyApp.ps1
@@ -1,27 +1,13 @@
 if ($env:APPVEYOR_REPO_BRANCH -ne "master")
 {
  Write-Host 'Skip "HockeyApp" deployment as no branches matched (build branch is "'"$env:APPVEYOR_REPO_BRANCH"'", deploy on branches "master")'
+ exit 0
 }
 
 Get-ChildItem .\Setup\Out\*.msi | ForEach-Object {
   $msi=$_.FullName
 
-  echo C:\ProgramData\Chocolatey\bin\curl -k `
-    -F "status=2" `
-    -F "notify=1" `
-    -F "commit_sha=$env:APPVEYOR_REPO_COMMIT" `
-    -F "build_server_url=https://ci.appveyor.com/project/$APPVEYOR_ACCOUNT_NAME/$APPVEYOR_PROJECT_NAME/build/$env:APPVEYOR_BUILD_VERSION" `
-    -F "repository_url=http://github.com/$APPVEYOR_REPO_NAME" `
-    -F "release_type=2" `
-    -F "notes=$APPVEYOR_REPO_COMMIT_MESSAGE" `
-    -F "notes_type=1" `
-    -F "mandatory=0" `
-    -F "teams=$env:HOCKEYAPP_TEAM_IDS" `
-    -F "ipa=$msi" `
-    -H "X-HockeyAppToken: REDACTED" `
-    https://rink.hockeyapp.net/api/2/apps/REDACTED/app_versions/upload
-
-  & C:\ProgramData\Chocolatey\bin\curl -k `
+  & C:\ProgramData\Chocolatey\bin\curl -sLk `
     -F "status=2" `
     -F "notify=1" `
     -F "commit_sha=$env:APPVEYOR_REPO_COMMIT" `
