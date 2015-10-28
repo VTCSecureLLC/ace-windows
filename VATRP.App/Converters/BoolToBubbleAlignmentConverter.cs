6c0ee28d85f51bdd3330f3687a039dd35cd7e135
Ruben Semerjyan <semrubo@gmail.com>
2015-10-29 02:24:27 +0400

    Changed bubble by default horizontal alignment

--- VATRP.App/Converters/BoolToBubbleAlignmentConverter.cs
+++ VATRP.App/Converters/BoolToBubbleAlignmentConverter.cs
@@ -9,24 +9,26 @@ namespace VATRP.App.Converters
     public class BoolToBubbleAlignmentConverter : IValueConverter
     {
         public HorizontalAlignment FalseEquivalent { get; set; }
+        public HorizontalAlignment TrueEquivalent { get; set; }
 
         public bool OppositeBooleanValue { get; set; }
 
         public BoolToBubbleAlignmentConverter()
         {
             this.FalseEquivalent = HorizontalAlignment.Right;
+            this.TrueEquivalent = HorizontalAlignment.Left;
             this.OppositeBooleanValue = false;
         }
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            bool booleanValue = false;
+            bool booleanValue = true;
             if (value is MessageDirection && (MessageDirection) value == MessageDirection.Outgoing)
             {
                 booleanValue = !OppositeBooleanValue;
             }
 
-            return booleanValue ? HorizontalAlignment.Left : FalseEquivalent;
+            return booleanValue ? TrueEquivalent : FalseEquivalent;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
