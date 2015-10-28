55a565fdda2a413c5838a4b98a61bb1829e6933d
Ruben Semerjyan <semrubo@gmail.com>
2015-10-29 02:24:50 +0400

    Changed outgoing bubble color

--- VATRP.App/Converters/BoolToBubbleColorConverter.cs
+++ VATRP.App/Converters/BoolToBubbleColorConverter.cs
@@ -14,7 +14,7 @@ namespace VATRP.App.Converters
         {
             if (value is MessageDirection && (MessageDirection)value == MessageDirection.Outgoing)
             {
-                return new SolidColorBrush(Color.FromArgb(0xff, 239, 226, 255));
+                return new SolidColorBrush(Color.FromArgb(255, 171, 248, 221));
             }            
             return new SolidColorBrush(Color.FromArgb(0xff, 237, 237, 237));
             
