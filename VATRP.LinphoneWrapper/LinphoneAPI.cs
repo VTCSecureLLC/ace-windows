4afed38fd04901c6dacffdffee910c2894ce298a
Ruben Semerjyan <semrubo@gmail.com>
2015-11-04 00:58:58 +0400

    Added methods wrappers for adaptive rate control

--- VATRP.LinphoneWrapper/LinphoneAPI.cs
+++ VATRP.LinphoneWrapper/LinphoneAPI.cs
@@ -863,6 +863,18 @@ namespace VATRP.LinphoneWrapper
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         public static extern int linphone_core_get_avpf_rr_interval(IntPtr lc);
 
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void linphone_core_enable_adaptive_rate_control(IntPtr lc, bool enabled);
+        
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool linphone_core_adaptive_rate_control_enabled( IntPtr lc);
+        
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void linphone_core_set_adaptive_rate_algorithm(IntPtr lc, string algorithm);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr linphone_core_get_adaptive_rate_algorithm(IntPtr lc);
+
         /**
  * Returns the list of available audio codecs.
  * @param[in] lc The LinphoneCore object
