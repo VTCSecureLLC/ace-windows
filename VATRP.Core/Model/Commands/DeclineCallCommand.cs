632f7474c0af28a55db6b0d06b097d184d1b55e7
Ruben Semerjyan <semrubo@gmail.com>
2015-11-29 15:39:10 +0400

    Added Linphone command classes

---
+++ VATRP.Core/Model/Commands/DeclineCallCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using VATRP.LinphoneWrapper.Enums;
+
+namespace VATRP.Core.Model.Commands
+{
+    internal class DeclineCallCommand : CallCommand
+    {
+        private readonly LinphoneReason _reason;
+
+        public DeclineCallCommand(IntPtr callPtr, LinphoneReason reason)
+            : base(LinphoneCommandType.DeclineCall, callPtr)
+        {
+            _reason = reason;
+        }
+
+        public LinphoneReason Reason
+        {
+            get { return _reason; }
+        }
+    }
+}
