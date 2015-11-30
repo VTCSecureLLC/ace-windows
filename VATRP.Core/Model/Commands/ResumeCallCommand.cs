4c2ef1aeb470cf18c36bf96fb4ebda2aa74efb0f
Ruben Semerjyan <semrubo@gmail.com>
2015-12-01 01:24:34 +0400

    Added ResumeCall and PauseCall commands

---
+++ VATRP.Core/Model/Commands/ResumeCallCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VATRP.Core.Model.Commands
+{
+    internal class ResumeCallCommand : CallCommand
+    {
+        public ResumeCallCommand(IntPtr callPtr)
+            : base(LinphoneCommandType.ResumeCall, callPtr)
+        {
+            
+        }
+    }
+}
