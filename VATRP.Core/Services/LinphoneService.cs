d78651c70c054a206cf09cfba03a9f962908e801
Shareef Ali <shareefalis@gmail.com>
2015-12-10 15:00:16 -0500

    Update linphone core libraries
    VATRP-1283 and VATRP-1195

--- VATRP.Core/Services/LinphoneService.cs
+++ VATRP.Core/Services/LinphoneService.cs
@@ -401,8 +401,8 @@ namespace VATRP.Core.Services
 
             if (callsDefaultParams != IntPtr.Zero)
             {
-                Marshal.FreeHGlobal(callsDefaultParams);
-                //LinphoneAPI.linphone_call_params_destroy(callsDefaultParams);
+                //Marshal.FreeHGlobal(callsDefaultParams);
+                LinphoneAPI.linphone_call_params_destroy(callsDefaultParams);
             }
             LinphoneAPI.linphone_core_iterate(linphoneCore); // roll
             
@@ -415,8 +415,8 @@ namespace VATRP.Core.Services
                 RegistrationStateChangedEvent(LinphoneRegistrationState.LinphoneRegistrationCleared);
 
             
-            Marshal.FreeHGlobal(linphoneCore);
-            //LinphoneAPI.linphone_core_destroy(linphoneCore);
+            //Marshal.FreeHGlobal(linphoneCore);
+            LinphoneAPI.linphone_core_destroy(linphoneCore);
             registration_state_changed = null;
             call_state_changed = null;
             notify_received = null;
