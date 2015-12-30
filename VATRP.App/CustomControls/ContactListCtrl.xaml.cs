17403a28cd6ea5dce86ab18ab3b2742c90dad2df
Liz E <lizann.epley@vtcsecure.com>
2015-12-30 17:43:31 -0500

    VATRP-1621: Fixing call from contact

--- VATRP.App/CustomControls/ContactListCtrl.xaml.cs
+++ VATRP.App/CustomControls/ContactListCtrl.xaml.cs
@@ -62,7 +62,7 @@ namespace com.vtcsecure.ace.windows.CustomControls
             if (_contactsViewModel.SelectedContact != null)
             {
                 if (MakeCallRequested != null && _contactsViewModel.SelectedContact.Contact != null)
-                    MakeCallRequested(_contactsViewModel.SelectedContact.Contact.SipUsername);
+                    MakeCallRequested(_contactsViewModel.SelectedContact.Contact.RegistrationName);
                 _contactsViewModel.SelectedContact = null;
             }
         }
