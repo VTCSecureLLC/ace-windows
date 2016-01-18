6c4fa5cb7ad02ba2a6dde7f4a82c91af791fc27b
Ruben Semerjyan <semrubo@gmail.com>
2016-01-18 23:39:39 +0400

    Revert "Check RTT button on receiving text message"
    This reverts commit c42a85482de4dfd284c72e4300edbc2a38d72241.

--- VATRP.App/CustomControls/CallViewCtrl.xaml.cs
+++ VATRP.App/CustomControls/CallViewCtrl.xaml.cs
@@ -644,12 +644,5 @@ namespace com.vtcsecure.ace.windows.CustomControls
                 _inactivityTimerStopped = false;
             }
         }
-        
-        public void CheckRttButton()
-        {
-            _viewModel.IsRttOn = true;
-            BtnRTT.IsChecked = _viewModel.IsRttOn;
-            _viewModel.SavedIsRttOn = BtnRTT.IsChecked ?? false;
-        }
     }
 }
