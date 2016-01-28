dc1f84c1cb96d9a1e815eea3339cc129407f9428
Liz E <lizann.epley@vtcsecure.com>
2016-01-27 21:09:13 -0500

    VATRP-63: Added Provider logos to provider combo on login. Default provider set to ACE

--- VATRP.Core/Services/ProviderService.cs
+++ VATRP.Core/Services/ProviderService.cs
@@ -285,5 +285,10 @@ namespace VATRP.Core.Services
 
             return providers.ToArray();
         }
+
+        public List<VATRPServiceProvider> GetProviderListFullInfo()
+        {
+            return providersList;
+        }
     }
 }
