cf25cf324eb77cc3746eba666634b53785afd8df
Liz E <lizann.epley@vtcsecure.com>
2016-01-28 23:01:49 -0500

    VATRP-1670: adding os name conversion to 'friendly' name

--- VATRP.App/CustomControls/UnifiedSettings/TechnicalSupportSheetCtrl.xaml.cs
+++ VATRP.App/CustomControls/UnifiedSettings/TechnicalSupportSheetCtrl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using com.vtcsecure.ace.windows.Utilities;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,10 +34,33 @@ namespace com.vtcsecure.ace.windows.CustomControls.UnifiedSettings
 
             this.ACEVersionLabel.Content = assembly.GetName().Name + " Version: ";
             this.ACEVersionInfoLabel.Content = string.Format("Version {0}.{1}.{2}", version.Major, version.Minor, version.Build);
+            OperatingSystem os = Environment.OSVersion;
 
-            this.OperatingSystemInfoLabel.Text = Environment.OSVersion.VersionString;
+            this.OperatingSystemInfoLabel.Text = FriendlyName() + " " + os.ServicePack;// Environment.OSVersion.VersionString;
 
             this.TechnicalSupprtInfoTextBlock.Text = TechnicalSupportInfoBuilder.GetStringForTechnicalSupprtString(false);
+        }
+        public string HKLM_GetString(string path, string key)
+        {
+            try
+            {
+                RegistryKey rk = Registry.LocalMachine.OpenSubKey(path);
+                if (rk == null) return "";
+                return (string)rk.GetValue(key);
+            }
+            catch { return ""; }
+        }
+
+        public string FriendlyName()
+        {
+            string ProductName = HKLM_GetString(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ProductName");
+            string CSDVersion = HKLM_GetString(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion", "CSDVersion");
+            if (ProductName != "")
+            {
+                return (ProductName.StartsWith("Microsoft") ? "" : "Microsoft ") + ProductName +
+                            (CSDVersion != "" ? " " + CSDVersion : "");
+            }
+            return "";
         }
     }
 }
