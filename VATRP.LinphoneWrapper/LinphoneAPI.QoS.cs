82696e626a1c491089be6cf538cf3922ec9bf82d
Ruben Semerjyan <semrubo@gmail.com>
2016-02-28 23:59:30 +0400

    Added Quality of service update support

---
+++ VATRP.LinphoneWrapper/LinphoneAPI.QoS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using VATRP.LinphoneWrapper.Enums;
+
+namespace VATRP.LinphoneWrapper
+{
+    public static partial class LinphoneAPI
+    {
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void linphone_core_set_sip_dscp(IntPtr lc, int dscp);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int linphone_core_get_sip_dscp(IntPtr lc);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void linphone_core_set_audio_dscp(IntPtr lc, int dscp);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int linphone_core_get_audio_dscp(IntPtr lc);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void linphone_core_set_video_dscp(IntPtr lc, int dscp);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int linphone_core_get_video_dscp(IntPtr lc);
+
+    }
+}
