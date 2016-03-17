2bd2987c19a1a1b5bf211a39f0a4aafdf55bc6a8
kenneth dick <kenneth.dick@vtcsecure.com>
2016-03-17 15:08:41 -0400

    hide theme selectors, change popup text

--- VATRP.App/CustomControls/UnifiedSettings/UnifiedSettingsThemeCtrl.xaml.cs
+++ VATRP.App/CustomControls/UnifiedSettings/UnifiedSettingsThemeCtrl.xaml.cs
@@ -48,7 +48,8 @@ namespace com.vtcsecure.ace.windows.CustomControls.UnifiedSettings
         }
         private void OnForce508(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("Force 508 Clicked");
+            Console.WriteLine("Coming Soon: High Contrast Theme");
+
         }
 
     }
