c57fbb3b159715d4c65a4c2b49700710ad96dbf5
Tigran Merelyan <tigran@accontech.com>
2016-04-22 01:51:11 +0400

    Revert "Changed call trying state description"
    This reverts commit 8999f1be6e7ccba10d20e9d33d7cdc2a27da3a3c.

--- VATRP.App/Converters/CallStateToStringConverter.cs
+++ VATRP.App/Converters/CallStateToStringConverter.cs
@@ -23,7 +23,7 @@ namespace com.vtcsecure.ace.windows.Converters
                 switch (state)
                 {
                     case VATRPCallState.Trying:
-                        return "Connecting...";
+                        return "Trying";
                     case VATRPCallState.Ringing:
                         return "Ringing";
                     case VATRPCallState.InProgress:
